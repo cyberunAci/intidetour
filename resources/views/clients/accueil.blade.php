3126cfe0387803433604a92c6a69e6a948f77030
Darcoeur974 <darcoeur136dark@gmail.com>
2020-01-07 15:54:46 +0400

    US44 (MF) : mise en place environement : Wireframe et debut de page daccueil

--- resources/views/clients/accueil.blade.php
+++ resources/views/clients/accueil.blade.php
@@ -5,6 +5,9 @@
     <h1>INTIDETOUR</h1>
     <a href="apropos">À propos</a>
     <a href="contact">Contact</a>
+    
+    <img src="https://via.placeholder.com/600" alt="">
+    
 </body>
 </html>
 
