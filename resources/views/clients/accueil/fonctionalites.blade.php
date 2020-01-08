a2d89fa9bb8d7775ac8542e61d23c27335e0d242
Darcoeur974 <darcoeur136dark@gmail.com>
2020-01-08 15:49:07 +0400

    US44 (MF) : mise en place CSS pour Accueil

--- resources/views/clients/accueil/fonctionalites.blade.php
+++ resources/views/clients/accueil/fonctionalites.blade.php
@@ -3,55 +3,55 @@
         <h2>Fonctionalitées</h2>
     </div>
     <div class="col-lg-9">
-        <div class="row">
+        <div class="row fonctionalitesRow">
             <div class="col-lg-3">
-                <div class="row">
+                <div class="fonctionalitesBlock">
                     <img src="https://via.placeholder.com/50" class="img-fluid" alt="">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <p class="fonctionalitesTexte">Lorem ipsum dolor sit</p>
                 </div>
             </div>
             <div class="col-lg-3">
-                <div class="row">
+                <div class="fonctionalitesBlock">
                     <img src="https://via.placeholder.com/50" class="img-fluid" alt="">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <p class="fonctionalitesTexte">Lorem ipsum dolor sit</p>
                 </div>
             </div>
             <div class="col-lg-3">
-                <div class="row">
+                <div class="fonctionalitesBlock">
                     <img src="https://via.placeholder.com/50" class="img-fluid" alt="">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <p class="fonctionalitesTexte">Lorem ipsum dolor sit</p>
                 </div>
             </div>
             <div class="col-lg-3">
-                <div class="row">
+                <div class="fonctionalitesBlock">
                     <img src="https://via.placeholder.com/50" class="img-fluid" alt="">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <p class="fonctionalitesTexte">Lorem ipsum dolor sit</p>
                 </div>
             </div>
         </div>
-        <div class="row">
+        <div class="row fonctionalitesRow">
             <div class="col-lg-3">
-                <div class="row">
+                <div class="fonctionalitesBlock">
                     <img src="https://via.placeholder.com/50" class="img-fluid" alt="">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <p class="fonctionalitesTexte">Lorem ipsum dolor sit</p>
                 </div>
             </div>
             <div class="col-lg-3">
-                <div class="row">
+                <div class="fonctionalitesBlock">
                     <img src="https://via.placeholder.com/50" class="img-fluid" alt="">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <p class="fonctionalitesTexte">Lorem ipsum dolor sit</p>
                 </div>
             </div>
             <div class="col-lg-3">
-                <div class="row">
+                <div class="fonctionalitesBlock">
                     <img src="https://via.placeholder.com/50" class="img-fluid" alt="">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <p class="fonctionalitesTexte">Lorem ipsum dolor sit</p>
                 </div>
             </div>
             <div class="col-lg-3">
-                <div class="row">
+                <div class="fonctionalitesBlock">
                     <img src="https://via.placeholder.com/50" class="img-fluid" alt="">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <p class="fonctionalitesTexte">Lorem ipsum dolor sit</p>
                 </div>
             </div>
         </div>
