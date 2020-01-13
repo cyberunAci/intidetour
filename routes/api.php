a6e4a426bd8de79c6be27623454767a2b0182ea5
Darcoeur974 <darcoeur136dark@gmail.com>
2020-01-13 15:54:29 +0400

    US-50 (MORGAN) : fichier de migration crée pour Success, manque la migration

--- routes/api.php
+++ routes/api.php
@@ -25,13 +25,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::prefix('circuits')->group(function () {
     Route::get('/', 'CircuitsController@index');
     Route::post('/add', 'CircuitsController@add');
-<<<<<<< HEAD
-    Route::delete('/{id}', 'CircuitController@delete')->where('id', "[0-9]+");
-    Route::post('/{id}/trace/add', 'CircuitController@addTrace')->where('id', "[0-9]+");
-=======
+    Route::post('/{id}/trace/add', 'CircuitsController@addTrace')->where('id', "[0-9]+");
     Route::get('/{id}', 'CircuitsController@show')->where('id', "[0-9]+");
     Route::post('/{id}', 'CircuitsController@update')->where('id', '[0-9]+');
     Route::delete('/{id}', 'CircuitsController@delete')->where('id', "[0-9]+");
    
->>>>>>> 685d2a1df0359e5366fe23d0a4ee9b2a8cecd4d4
 });
