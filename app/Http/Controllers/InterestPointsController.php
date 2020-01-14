014367ad16c7735e13299b5b72d70ce8afbc2907
KrishnaPermal <Krishna.tantalidespermalnaiken@gmail.com>
2020-01-14 16:01:10 +0400

    us35 krishna interestpoints finish

--- app/Http/Controllers/InterestPointsController.php
+++ app/Http/Controllers/InterestPointsController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Request;
 
 class InterestPointsController extends Controller
 {
-<<<<<<< HEAD
    
     /**
      * Function recuperations de tous les InterestPoints
@@ -27,30 +26,30 @@ class InterestPointsController extends Controller
 
 
 
-    /**
-     * Function ajout d'un InterestPoints
-     *
-     * @param Request $request requete d'entrée
-     * @return retourne le interestpoints entré en BDD
-     */
-    public function add(Request $request)
-    {
-        //Validation des données entrées
-        $dataInterestPoints = Validator::make(
-            $request->input(),
-            [
-                'nom' => 'required',
-                'coordonnees' => 'required',
-            ],
-            [
-                'required' => 'Le champs :attribute est requis', // :attribute renvoie le champs / l'id de l'element en erreur
-            ]
-        )->validate();
-        //Ajout en bdd des données validées par le validator
-        $interestPoints = InterestPointsModel::create($dataInterestPoints);
-        //Retourne l'interestpoints formaté grace à la ressource
-        return new InterestPointsRessource($interestPoints);
-    }
+    // /**
+    //  * Function ajout d'un InterestPoints
+    //  *
+    //  * @param Request $request requete d'entrée
+    //  * @return retourne le interestpoints entré en BDD
+    //  */
+    // public function add(Request $request)
+    // {
+    //     //Validation des données entrées
+    //     $dataInterestPoints = Validator::make(
+    //         $request->input(),
+    //         [
+    //             'nom' => 'required',
+    //             'coordonnees' => 'required',
+    //         ],
+    //         [
+    //             'required' => 'Le champs :attribute est requis', // :attribute renvoie le champs / l'id de l'element en erreur
+    //         ]
+    //     )->validate();
+    //     //Ajout en bdd des données validées par le validator
+    //     $interestPoints = InterestPointsModel::create($dataInterestPoints);
+    //     //Retourne l'interestpoints formaté grace à la ressource
+    //     return new InterestPointsRessource($interestPoints);
+    // }
 
 
     }
@@ -58,15 +57,3 @@ class InterestPointsController extends Controller
 
 
 
-=======
-    /**
-     * Function qui affiche les points d'intérêt important 
-     * @param 
-     * @return 
-     */
-    public function interestsPointsTypes()
-    {
-        
-    }
-}
->>>>>>> 0f1923715aadb26cc9f63a50d4cdd53c8cc5f30b
