4db9df24741cfe0a3b3860bf58baa7852ee73656
kevin97426 <kevin.jams974@gmail.com>
2020-01-20 14:19:00 +0400

    US-30: En tant qu'admin je veux pouvoir update les informations du à propos

---
+++ database/migrations/2020_01_20_101340_create_apropos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAproposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apropos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apropos');
+    }
+}
