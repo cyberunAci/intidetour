8998de3d0d0055e28618ef0fe5cb91eeb0c8de55
Darcoeur974 <darcoeur136dark@gmail.com>
2020-02-04 15:36:51 +0400

    suppression de public/app.js, ajout gitignore

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,2 @@
 require('./bootstrap');
-<<<<<<< HEAD
 require('./dashboard/app');
-=======
-require('./dashboard/app');
-
-// import 'owl.carousel/dist/assets/owl.carousel.css';
-// import 'owl.carousel';
-
-// $(document).ready(function(){
-//     $('.owl-carousel').owlCarousel();
-//   });
->>>>>>> 38783ef73d5906f41ccdbd371c123aadf6c6ae85
