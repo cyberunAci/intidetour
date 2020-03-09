3462eb5001a2341256b7042a6d0da871d4865a71
KrishnaPermal <Krishna.tantalidespermalnaiken@gmail.com>
2020-03-09 13:53:07 +0400

    modif galerie accueil

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -14,7 +14,7 @@ return array(
     'App\\Http\\Controllers\\AboutController' => $baseDir . '/app/Http/Controllers/AboutController.php',
     'App\\Http\\Controllers\\AccueilController' => $baseDir . '/app/Http/Controllers/AccueilController.php',
     'App\\Http\\Controllers\\AdminController' => $baseDir . '/app/Http/Controllers/AdminController.php',
-    'App\\Http\\Controllers\\AuthConttroller' => $baseDir . '/app/Http/Controllers/AuthConttroller.php',
+    'App\\Http\\Controllers\\AuthController' => $baseDir . '/app/Http/Controllers/AuthController.php',
     'App\\Http\\Controllers\\Auth\\ConfirmPasswordController' => $baseDir . '/app/Http/Controllers/Auth/ConfirmPasswordController.php',
     'App\\Http\\Controllers\\Auth\\ForgotPasswordController' => $baseDir . '/app/Http/Controllers/Auth/ForgotPasswordController.php',
     'App\\Http\\Controllers\\Auth\\LoginController' => $baseDir . '/app/Http/Controllers/Auth/LoginController.php',
