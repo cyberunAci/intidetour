16f30068ef99be3774d2704194edc85b24399733
Darcoeur974 <darcoeur136dark@gmail.com>
2020-05-28 14:50:19 +0400

    middleware en cours de mise en place

--- resources/js/intidetour/_services/api.services.js
+++ resources/js/intidetour/_services/api.services.js
@@ -1,5 +1,3 @@
-import { authenticationService } from '../_services/authentication.service'
-
 import Axios from "axios"
 
 
@@ -12,35 +10,15 @@ export const apiServices = {
             params: params
         });
     },
-    post(url, body, params = {}) {
+    post(url, params = {}) {
         return Axios({
-            method: 'POST',
+            method: 'post',
             url: url,
             headers: headers(),
-            data: JSON.stringify(body)
         });
     },
-    patch(body) {
-        return {
-            method: 'PATCH',
-            ...headers(),
-            body: JSON.stringify(body)
-        };
-    },
-    put(body) {
-        return {
-            method: 'PUT',
-            ...headers(),
-            body: JSON.stringify(body)
-        };
-    },
-    delete() {
-        return {
-            method: 'DELETE',
-            ...headers()
-        };
-    }
 }
+
 function headers() {
     const currentUser = authenticationService.currentUserValue || {};
     const authHeader = currentUser.token ? { 'Authorization': 'Bearer ' + currentUser.token } : {}
