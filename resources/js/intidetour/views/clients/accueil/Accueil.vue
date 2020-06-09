2661de772578eb1b36b0b27251af0fb406800059
Darcoeur974 <darcoeur136dark@gmail.com>
2020-06-09 10:05:16 +0400

    front accueil, responsive

--- resources/js/intidetour/views/clients/accueil/Accueil.vue
+++ resources/js/intidetour/views/clients/accueil/Accueil.vue
@@ -2,31 +2,43 @@
   <v-container fluid class="ma-0 pa-0">
     <v-row class="ma-0 pa-0">
       <v-col class="ma-0 pa-0">
-          <v-img src="https://via.placeholder.com/1000x450" alt class="align-center mb-2 pa-1" width="auto" height="750px">
-            <v-col cols="12" md="5" sm="12" class="align-center">
-              <v-row>
-                <h2>C'est quoi Intidetour ?</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo beatae reiciendis, saepe illum velit dignissimos doloremque quis fugiat hic tempore animi rem error possimus nisi repellat soluta laborum. Facilis, consectetur.</p>
-              </v-row>
-              <v-row>
-                <h3>Installer Intidetour ?</h3>
-              </v-row>
-              <v-row>
-                <v-col>
-                  <v-btn color="primary" width="100%">Google Play</v-btn>
-                </v-col>
-                <v-col>
-                  <v-btn color="primary" width="100%">App Store</v-btn>
-                </v-col>
-              </v-row>
-            </v-col>
-          </v-img>
+        <v-img
+          src="https://via.placeholder.com/1000x450"
+          alt
+          class="align-center mb-2 pa-1"
+          width="auto"
+          height="750px"
+        >
+          <v-col cols="12" md="5" sm="12" class="align-center">
+            <v-row>
+              <h2>C'est quoi Intidetour ?</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo beatae reiciendis, saepe illum velit dignissimos doloremque quis fugiat hic tempore animi rem error possimus nisi repellat soluta laborum. Facilis, consectetur.</p>
+            </v-row>
+            <v-row>
+              <h3>Installer Intidetour ?</h3>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-btn color="primary" width="100%">Google Play</v-btn>
+              </v-col>
+              <v-col>
+                <v-btn color="primary" width="100%">App Store</v-btn>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-img>
       </v-col>
     </v-row>
     <equipe></equipe>
+    <section class="d-flex flex-column justify-center ma-2">
+      <hr />
+    </section>
     <circuits></circuits>
     <fonctionnalite></fonctionnalite>
     <Newsletter></Newsletter>
+    <section class="d-flex flex-column justify-center ma-2">
+      <hr />
+    </section>
     <temoignages></temoignages>
   </v-container>
 </template>
